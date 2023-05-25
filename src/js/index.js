fed11873a4d8e1ea213001692bfba2371d8cdea6
Laura Comte <comtelaurapro@gmail.com>
2023-05-25 11:35:18 +0200

    js bascis

---
+++ src/js/index.js
@@ -0,0 +1,12 @@
+const x = "13.25px";
+const y = false;
+const z = -654 / 6;
+const result = (z + y) * x;
+console.log(result);
+
+let color;
+color = "red";
+console.log(color);
+
+const msg = "bisous";
+console.log(msg);
