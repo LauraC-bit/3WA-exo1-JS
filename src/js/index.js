d00a3ed1a55295e0f2fa2e67350a61b792966b4c
Laura Comte <comtelaurapro@gmail.com>
2023-05-31 12:36:40 +0200

    function

--- src/js/index.js
+++ src/js/index.js
@@ -1,76 +1,76 @@
-const x = "13.25px";
-const y = false;
-const z = -654 / 6;
-const result = (z + y) * x;
-console.log(result);
+// const x = "13.25px";
+// const y = false;
+// const z = -654 / 6;
+// const result = (z + y) * x;
+// console.log(result);
 
-let color;
-color = "red";
-console.log(color);
+// let color;
+// color = "red";
+// console.log(color);
 
-const msg = "bisous";
-console.log(msg);
+// const msg = "bisous";
+// console.log(msg);
 
-// const a = 11 / "Salut";
-// console.log(a);
-// console.log(typeof a);
+// // const a = 11 / "Salut";
+// // console.log(a);
+// // console.log(typeof a);
 
-const word1 = "j'adore";
-const word2 = "les chats";
-const space = " ";
-console.log(word1 + " " + word2);
-console.log(word1 + space + word2);
-console.log(`${word1} ${word2}`);
+// const word1 = "j'adore";
+// const word2 = "les chats";
+// const space = " ";
+// console.log(word1 + " " + word2);
+// console.log(word1 + space + word2);
+// console.log(`${word1} ${word2}`);
 
-let results = new Date();
-console.log(results);
+// let results = new Date();
+// console.log(results);
 
-let day = results.getDate();
-let month = results.getMonth();
-let year = results.getFullYear();
+// let day = results.getDate();
+// let month = results.getMonth();
+// let year = results.getFullYear();
 
-const resultDate = `${day}-0${month + 1}-${year}`;
+// const resultDate = `${day}-0${month + 1}-${year}`;
 
-console.log(resultDate);
+// console.log(resultDate);
 
-const decimal = 100 / 3;
-const roundedValue = Math.round(decimal);
-console.log(roundedValue);
+// const decimal = 100 / 3;
+// const roundedValue = Math.round(decimal);
+// console.log(roundedValue);
 
-// --------------------------
+// // --------------------------
 
-const a = parseFloat("43px");
-const b = Number(false);
-const c = parseFloat(" 0.423");
-const d = 64.09;
+// const a = parseFloat("43px");
+// const b = Number(false);
+// const c = parseFloat(" 0.423");
+// const d = 64.09;
 
 // calcul suivant : (a + b) / (d - c);
 
-const somme = (a + b) / (d - c);
-console.log(Math.round(somme));
-const somme2 = Math.round(somme);
-let somme3 = somme2.toFixed(3);
-console.log(somme3);
+// const somme = (a + b) / (d - c);
+// console.log(Math.round(somme));
+// const somme2 = Math.round(somme);
+// let somme3 = somme2.toFixed(3);
+// console.log(somme3);
 
-let somme4 = Number(somme.toFixed(3));
-console.log(somme4);
+// let somme4 = Number(somme.toFixed(3));
+// console.log(somme4);
 
-// Du coup, comment fait-on pour créer un objet user qui a un nom, un prénom, un email, un age et une adresse qui est elle même un objet composé d'une rue, d'une ville et d'un code postal ?
+// // Du coup, comment fait-on pour créer un objet user qui a un nom, un prénom, un email, un age et une adresse qui est elle même un objet composé d'une rue, d'une ville et d'un code postal ?
 
-let Tartonfion = {
-  nom: "Tarton",
-  prenom: "Fion",
-  age: 99,
-  email: "tartonfion@gmail.com",
-  adresse: {
-    numero: 13,
-    rue: "avenue tsoin tsoin",
-    codepostal: 33000,
-    ville: "Bordeaux",
-  },
-};
+// let Tartonfion = {
+//   nom: "Tarton",
+//   prenom: "Fion",
+//   age: 99,
+//   email: "tartonfion@gmail.com",
+//   adresse: {
+//     numero: 13,
+//     rue: "avenue tsoin tsoin",
+//     codepostal: 33000,
+//     ville: "Bordeaux",
+//   },
+// };
 
-console.log(Tartonfion);
+// console.log(Tartonfion);
 
 // TABLEAU ARRAY
 
@@ -99,26 +99,26 @@ console.log(Tartonfion);
 
 // -----------------------------------------------------------------
 
-let car = {
-  doors: 4,
-  brand: "honda",
-  color: "red",
-  motor: {
-    power: 7,
-    weight: 100,
-    type: "vapeur",
-  },
-  passenger: [
-    {
-      name: "gisèle",
-      age: 33,
-      email: "gisele.truc@car.com",
-    },
-  ],
-};
+// let car = {
+//   doors: 4,
+//   brand: "honda",
+//   color: "red",
+//   motor: {
+//     power: 7,
+//     weight: 100,
+//     type: "vapeur",
+//   },
+//   passenger: [
+//     {
+//       name: "gisèle",
+//       age: 33,
+//       email: "gisele.truc@car.com",
+//     },
+//   ],
+// };
 
-console.log(car);
-console.log(car.motor);
+// console.log(car);
+// console.log(car.motor);
 
 // -----------------------------------------------------------------
 // const array = [34, 87, 72, 12, 54];
@@ -145,12 +145,12 @@ console.log(car.motor);
 
 // -------------------------------------------------
 
-let array = [];
-const max = 200;
-const min = 1;
+// let array = [];
+// const max = 200;
+// const min = 1;
 
-let smaller = 200;
-let bigger = 1;
+// let smaller = 200;
+// let bigger = 1;
 // let array = getRandomInt();
 
 // function getRandomInt(min, max) {
@@ -159,19 +159,69 @@ let bigger = 1;
 //   return Math.floor(Math.random() * (max - min + 1)) + min;
 // }
 
-for (let i = 0; i < 20; i++) {
-  const random = Math.floor(Math.random() * (max - min + 1)) + min;
-  array.push(random);
-}
+// for (let i = 0; i < 20; i++) {
+//   const random = Math.floor(Math.random() * (max - min + 1)) + min;
+//   array.push(random);
+// }
 
-for (let i = 0; i < array.length; i++) {
-  const numbers = array[i];
-  if (numbers < smaller) {
-    smaller = numbers;
+// for (let i = 0; i < array.length; i++) {
+//   const numbers = array[i];
+//   if (numbers < smaller) {
+//     smaller = numbers;
+//   }
+//   if (numbers > bigger) {
+//     bigger = numbers;
+//   }
+// }
+// console.log(`Le plus grand nombre est ${bigger}`);
+// console.log(`Le plus petit nombre est ${smaller}`);
+
+// -------------------------------------------------------------------
+
+const array = [34, 87, 72, 12, 54];
+
+const double = (a) => {
+  return a * 2;
+};
+
+// let doubleTab = [];
+
+// for (let i = 0; i < array.length; i++) {
+//   let b = array[i];
+//   const doubled = double(b);
+//   console.log(doubled);
+//   doubleTab.push(doubled);
+// }
+
+// console.log(doubleTab);
+
+// console.log(array);
+
+// -------------------------------------------------------------------------
+
+// const arrayX2 = (a) => {
+//   return a * 2;
+// };
+
+// let newArray = [arrayX2(array)];
+// console.log(newArray);
+
+const triple = (a) => {
+  return a * 3;
+};
+
+const tabFunc = (tab, func) => {
+  const newTab = [];
+
+  for (let i = 0; i < tab.length; i++) {
+    const nbr = tab[i];
+    const newNbr = func(nbr);
+    newTab.push(newNbr);
   }
-  if (numbers > bigger) {
-    bigger = numbers;
-  }
-}
-console.log(`Le plus grand nombre est ${bigger}`);
-console.log(`Le plus petit nombre est ${smaller}`);
+  return newTab;
+};
+
+const newTab = tabFunc(array, triple);
+console.log(newTab);
+
+// Transformer la fonction tabFunc ci-dessus pour prendre en 2eme parametre une fonction qui s'executera sur chacun des elements du tableau (a la place de la fonction double)
